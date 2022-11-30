eb0e0e73ff194cb27a9a9a52b3ff317d2d9484a9
Ivanreet Kaur <97057161+ivanreet507@users.noreply.github.com>
2022-11-29 21:28:11 -0800

    Create array_list.h

---
+++ array_list.h
@@ -0,0 +1,8 @@
+typedef struct ArrayList{
+    int length;
+    int capacity;
+    char **elements;
+} ArrayList;
+
+ArrayList *array_list_new();
+void array_list_add_to_end(ArrayList *list, char *string);
