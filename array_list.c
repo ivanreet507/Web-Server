a186fafcda54b3613505114ebe5de3c440bdc626
Ivanreet Kaur <97057161+ivanreet507@users.noreply.github.com>
2022-11-29 21:27:55 -0800

    Create array_list.c

---
+++ array_list.c
@@ -0,0 +1,27 @@
+#define _GNU_SOURCE
+#include<stdio.h>
+#include<fcntl.h>
+#include<stdint.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include "array_list.h"
+
+ArrayList *array_list_new() {
+  char **str = malloc(5 * sizeof(char *));
+  ArrayList *array_list = malloc(sizeof(ArrayList));
+  array_list -> length = 0;
+  array_list -> capacity = 10;
+  array_list -> elements = str;
+  return array_list;
+}
+
+void array_list_add_to_end(ArrayList *array_list, char *str){
+    if (array_list -> length >= array_list -> capacity){
+        array_list -> capacity = array_list -> capacity * 2;
+        array_list -> elements = realloc(array_list -> elements, array_list -> capacity * sizeof(char *));
+    }
+    array_list -> elements[array_list -> length] = malloc(strlen(str) + 1);
+    strcpy(array_list -> elements[array_list -> length], str);
+    array_list -> length = array_list -> length + 1;
+}
